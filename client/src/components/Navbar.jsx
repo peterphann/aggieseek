a6d7cea41c4bc4366e124c8edb48f08f223072d5
Sophia Phu <sophiatiffphu@gmail.com>
2024-03-03 03:10:56 -0600

    altered colors of navbar buttons and removed notification bell

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,10 +4,8 @@ import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import logo from '../assets/logo-white.png'
 
 const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
+  { name: 'Home', href: '#', current: true },
+  { name: 'Login', href: '#', current: false },
 ]
 
 function classNames(...classes) {
@@ -48,8 +46,8 @@ export default function Navbar() {
                         key={item.name}
                         href={item.href}
                         className={classNames(
-                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                          'rounded-md px-3 py-2 text-sm font-medium'
+                          item.current ? 'bg-[#FF977E] ' : 'bg-[#FBC7BA] ',
+                          'text-black rounded-[20px] hover:bg-gray-700 hover:text-white px-5 py-1.5 text-md font-medium'
                         )}
                         aria-current={item.current ? 'page' : undefined}
                       >
@@ -60,14 +58,6 @@ export default function Navbar() {
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button
-                  type="button"
-                  className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                >
-                  <span className="absolute -inset-1.5" />
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
 
                 {/* Profile dropdown */}
                 <Menu as="div" className="relative ml-3">
