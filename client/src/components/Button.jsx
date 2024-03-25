3991def9d8b5ef05374a7085fc618226e9c99a14
Peter <49101240+peterphann@users.noreply.github.com>
2024-03-25 11:58:54 -0500

    Update button component

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,8 +1,8 @@
-const Button = ({ children, onClick }) => {
+const Button = ({ children, ...props }) => {
   return (
     <button 
-    className="transition-transform ease-in-out duration-100 rounded-lg px-6 py-2 bg-white shadow-md text-black font-bold active:translate-y-1"
-    onClick={onClick}>
+    className="transition-transform ease-in-out duration-100 rounded-lg px-6 py-2 bg-white shadow-md text-black hover:bg-red-50 font-bold active:translate-y-1"
+    {...props}>
       {children}
     </button>
   );
