55c08b901108649c407091d3be000a8ed8e7777d
Peter <49101240+peterphann@users.noreply.github.com>
2024-03-25 16:07:34 -0500

    Polish signin and signup pages

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,32 +1,64 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Button from "../components/Button";
 import { Link } from "react-router-dom";
+import { auth } from "../firebase.jsx";
+import { signInWithEmailAndPassword } from "firebase/auth"
+import { useNavigate } from "react-router-dom";
+
+const errorMessages = {
+  "auth/invalid-email": "The email you entered is invalid.",
+  "auth/missing-password": "Please enter a password.",
+  "auth/invalid-credential": "The password you entered is incorrect.",
+  "auth/too-many-requests": "You have attempted to log in too many times. Please wait before trying again."
+}
 
 const Signin = () => {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
+  const [message, setMessage] = useState("")
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      if (user) {
+        navigate("/dashboard")
+      }
+    })
+  }, [])
 
   const handleSignin = (e) => {
-    e.preventDefault()
+    e.preventDefault();
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        navigate("/dashboard")
+      })
+      .catch((error) => {
+        const errorCode = error.code
+        setMessage(errorCode)
+      })
   }
 
   return (
     <div className="justify-center flex mt-20">
       <div className="flex-col items-center bg-white px-16 py-10 rounded-lg shadow-md">
         <h1 className="text-center font-bold text-black text-3xl">Login</h1>
-        <h2 className="text-center text-black text-sm font-medium">Don't have an account? <Link to="/signup" className="text-blue-700">Sign Up</Link></h2>
+        <h2 className="text-center text-black text-sm font-medium">Don't have an account? <Link to="/signup" className="text-blue-700 hover:underline">Sign Up</Link></h2>
 
         <form onSubmit={(e) => handleSignin(e)}>
           <div className="mt-5 text-center text-sm">
-            <input type="text" name="email" id="email" placeholder="Email" className="bg-white text-black border-b-2 border-zinc-200 px-6 py-2" autoComplete="off" />
+            <input type="text" name="email" id="email" placeholder="Email" onChange={(e) => {setEmail(e.target.value)}} value={email} className="bg-white text-black border-b-2 border-zinc-200 px-6 py-2" autoComplete="off" />
           </div>
 
           <div className="mt-5 text-center text-sm">
-            <input type="password" name="password" id="password" placeholder="Password" className="bg-white text-black border-b-2 border-zinc-200 px-6 py-2" />
-            <p>Forgot password?</p>
+            <input type="password" name="password" id="password" placeholder="Password" onChange={(e) => {setPassword(e.target.value)}} value={password} className="bg-white text-black border-b-2 border-zinc-200 px-6 py-2" />
           </div>
 
-          <div className="text-center mt-5">
+          <div className="mt-5 text-center">
+            <p className="text-red-600 font-medium text-xs min-h-6">{message}</p>
+          </div>
+
+          <div className="text-center mt-6">
             <Button action="submit">Login</Button>
           </div>
         </form>
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,37 +1,83 @@
+import { useState, useEffect } from "react";
 import Button from "../components/Button";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { auth } from "../firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth"
+
+const errorMessages = {
+  "auth/invalid-email": "The email you entered is invalid.",
+  "auth/missing-password": "Please enter a password.",
+  "auth/email-already-in-use": "The email you entered is already in use."
+}
 
 const Signup = () => {
+  const navigate = useNavigate()
+  const [first, setFirst] = useState("")
+  const [last, setLast] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [confirm, setConfirm] = useState("")
+  const [message, setMessage] = useState("")
+
+  const handleSignup = (e) => {
+    e.preventDefault()
+
+    if (password != confirm) {
+      setMessage("The passwords you entered do not match!")
+      return
+    }
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        navigate('/dashboard')
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        setMessage(errorMessages[errorCode])
+      })
+  }
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      if (user) {
+        navigate("/dashboard")
+      }
+    })
+  }, [])
+
   return (
     <div className="justify-center flex mt-20">
-      <div className="flex-col items-center bg-white px-16 py-10 rounded-lg shadow-md">
+      <div className="flex-col items-center bg-white px-20 py-10 rounded-lg shadow-md">
         <h1 className="text-center font-bold text-black text-3xl">Sign Up</h1>
-        <h2 className="text-center text-black text-sm font-medium">Already have an account? <Link to="/signin" className="text-blue-700">Login</Link></h2>
+        <h2 className="text-center text-black text-sm font-medium">Already have an account? <Link to="/signin" className="text-blue-700 hover:underline">Login</Link></h2>
 
-        <form onSubmit={(e) => handleSignin(e)}>
+        <form onSubmit={(e) => handleSignup(e)}>
           
-          <div className="mt-5 text-center">
-            <input type="text" name="firstName" id="firstName" placeholder="First Name" className="bg-white text-black border-b-2 border-zinc-200 px-6 py-2" autoComplete="address-level4" />
+          <div className="mt-5 text-center text-sm">
+            <input type="text" name="firstName" id="firstName" placeholder="First Name" onChange={(e) => setFirst(e.target.value)} value={first} className="bg-white text-black border-b-2 border-zinc-200 px-6 py-2" autoComplete="address-level4" />
+          </div>
+
+          <div className="mt-5 text-center text-sm">
+            <input type="text" name="lastName" id="lastName" placeholder="Last Name" value={last} onChange={(e) => setLast(e.target.value)} className="bg-white text-black border-b-2 border-zinc-200 px-6 py-2" autoComplete="address-level4" />
+          </div>
+
+          <div className="mt-5 text-center text-sm">
+            <input type="text" name="email" id="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} className="bg-white text-black border-b-2 border-zinc-200 px-6 py-2" autoComplete="address-level4" />
+          </div>
+
+          <div className="mt-5 text-center text-sm">
+            <input type="password" name="password" id="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} className="bg-white text-black border-b-2 border-zinc-200 px-6 py-2" />
+          </div>
+
+          <div className="mt-5 text-center text-sm">
+            <input type="password" name="confirmPass" id="confirmPass" placeholder="Confirm Password" onChange={(e) => setConfirm(e.target.value)} value={confirm} className="bg-white text-black border-b-2 border-zinc-200 px-6 py-2" />
           </div>
 
           <div className="mt-5 text-center">
-            <input type="text" name="lastName" id="lastName" placeholder="Last Name" className="bg-white text-black border-b-2 border-zinc-200 px-6 py-2" autoComplete="address-level4" />
+            <p className="text-red-600 font-medium text-xs min-h-6">{message}</p>
           </div>
 
-          <div className="mt-5 text-center">
-            <input type="text" name="email" id="email" placeholder="Email" className="bg-white text-black border-b-2 border-zinc-200 px-6 py-2" autoComplete="address-level4" />
-          </div>
-
-          <div className="mt-5 text-center">
-            <input type="password" name="password" id="password" placeholder="Password" className="bg-white text-black border-b-2 border-zinc-200 px-6 py-2" />
-          </div>
-
-          <div className="mt-5 text-center">
-            <input type="password" name="password" id="password" placeholder="Confirm Password" className="bg-white text-black border-b-2 border-zinc-200 px-6 py-2" />
-            <p>Forgot password?</p>
-          </div>
-
-          <div className="text-center mt-3">
+          <div className="text-center mt-5">
             <Button>Sign Up</Button>
           </div>
         </form>
