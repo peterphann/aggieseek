11ad3a62f4fefd03c939bcbe75b98586c1551d4d
Peter <49101240+peterphann@users.noreply.github.com>
2024-04-09 04:29:38 -0500

    Refactor code

--- server/server.py
+++ server/server.py
@@ -2,23 +2,18 @@ from flask import Flask, Response
 from flask_cors import CORS, cross_origin
 from section import scrape_section
 
-# API key and other parameters from Firebase
-config = {
-  
-}
-
 app = Flask(__name__)
 
 @app.route('/sections/<term>/<crn>/', methods=['GET'])
 @cross_origin(origin='*')
 def sections(term, crn):
-  
   course_info = scrape_section(term, crn)
 
   if course_info:
     return course_info
   else:
-    return Response(f'{{"error": "Course not found"}}', status=400, mimetype='application/json')
+    return Response(f'{{"error": "Course not found", "crn": {crn}}}', status=400, mimetype='application/json')
   
+
 if __name__ == "__main__":
   app.run(debug=True, port=8080)
