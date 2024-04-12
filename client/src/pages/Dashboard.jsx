c730c837927bec93ff0f2b01b4f8fdca6f098d8e
Peter <49101240+peterphann@users.noreply.github.com>
2024-04-12 05:19:42 -0500

    Retrieve section information from backend

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,11 +1,13 @@
-import {Table,
+import {
+  Table,
   TableBody,
   TableCaption,
   TableFooter,
   TableCell,
   TableHead,
   TableHeader,
-  TableRow, } from "../components/Table";
+  TableRow,
+} from "../components/Table";
 import { Menu } from '@headlessui/react';
 import {
   Pagination,
@@ -16,75 +18,30 @@ import {
   PaginationNext,
   PaginationPrevious,
 } from "../components/Pagination"
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
 
-const sections = [
-  {
-    crn: 47550,
-    section: 599,
-    term: "Fall 2024",
-    course: "CSCE 181",
-    title: "Introduction to Computing",
-    professor: "Tyagi, Aakash",
-    seats: 371
-  },
-  {
-    crn: 10937,
-    section: 501,
-    term: "Fall 2024",
-    course: "CSCE 312",
-    title: "Computer Organization",
-    professor: " Mahapatra, Rabinarayan",
-    seats: 22
-  },
-  {
-    crn: 49444,
-    section: 507,
-    term: "Fall 2024",
-    course: "MATH 304",
-    title: "Linear Algebra",
-    professor: "Not Assigned",
-    seats: 33
-  },
-  {
-    crn: 52795,
-    section: 514,
-    term: "Fall 2024",
-    course: "MATH 304",
-    title: "Linear Algebra",
-    professor: "Not Assigned",
-    seats: 0
-  },
-  {
-    crn: 57605,
-    section: 500,
-    term: "Fall 2024",
-    course: "DAEN 331",
-    title: "Optimization of Analytics",
-    professor: "Not Assigned",
-    seats: 35
-  },
-  {
-    crn: 58223,
-    section: 500,
-    term: "Fall 2024",
-    course: "FILM 475",
-    title: "Film, Propaganda, and Dissidence",
-    professor: "Cooke, Leighton",
-    seats: 20
-  }
-]
-
 const Dashboard = () => {
 
   const navigate = useNavigate()
+  const [crnInput, setCrnInput] = useState("")
+  const [crns, setCrns] = useState([47550, 10937, 49444, 57605, 58223, 52796, 57519])
+  const [sections, setSections] = useState([])
 
   const dashboardTest = () => {
     const db = getDatabase()
-  
+
+  }
+
+  const addSection = () => {
+    const crnAdd = parseInt(crnInput)
+    console.log(crns.includes(crnAdd))
+    if (!crns.includes(crnAdd)) {
+      setCrns(old => [...old, crnAdd])
+    }
+    
   }
 
   useEffect(() => {
@@ -98,11 +55,26 @@ const Dashboard = () => {
     })
   }, [])
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const responses = await Promise.all(
+        crns.map(async (crn) => {
+          const response = await fetch(`http://localhost:8080/sections/202431/${crn}/`);
+          return response.json()
+        })
+      );
+
+      setSections(responses)
+    }
+
+    fetchData()
+  }, [crns])
+
   return (
     <div>
-    <div className="flex justify-center items-center mt-[3%]"> {/* Fullscreen container for vertical & horizontal centering */}
-      <div className="flex items-center justify-center w-full max-w-4xl px-4 relative"> {/* Content container */}
-        <h2 className="text-3xl font-bold absolute w-full text-center">Sections I'm Watching</h2> {/* Absolutely positioned to center */}
+      <div className="flex justify-center items-center mt-[3%]"> {/* Fullscreen container for vertical & horizontal centering */}
+        <div className="flex items-center justify-center w-full max-w-4xl px-4 relative"> {/* Content container */}
+          <h2 className="text-3xl font-bold absolute w-full text-center">Sections I'm Watching</h2> {/* Absolutely positioned to center */}
           <div className="flex justify-end w-full"> {/* Container for right-aligned items */}
             <Menu as="div" className="relative inline-block text-left">
               <Menu.Button className="inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-[#8d0509] hover:underline focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
@@ -112,16 +84,16 @@ const Dashboard = () => {
                 <div className="px-1 py-1">
                   <Menu.Item>
                     {({ active }) => (
-                      <form  className={` p-2`}>
-                      <label className="block text-sm font-medium text-center text-gray-700">Enter your desired CRN</label>
-                      <input onClick={(e) => e.stopPropagation()} type="text" name="crn" id="crn" placeholder = "CRN" autoComplete="off" className="mt-2 block w-full h-8 rounded-md border-1 shadow-sm sm:text-sm px-2" />
-                      <div className="flex justify-center w-full">
-                      <button type="submit" className="mt-3 inline-flex justify-center border border-transparent bg-[#8d0509] py-2 px-3 text-sm font-medium text-white shadow-sm hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2">
-                        Track this section
-                      </button>
-                      </div>
+                      <form className={` p-2`}>
+                        <label className="block text-sm font-medium text-center text-gray-700">Enter your desired CRN</label>
+                        <input value={crnInput} onChange={(e) => setCrnInput(e.target.value)} onClick={(e) => e.stopPropagation()} type="number" name="crn" id="crn" placeholder="CRN" autoComplete="off" className="mt-2 block w-full h-8 rounded-md border-1 shadow-sm sm:text-sm px-2" />
+                        <div className="flex justify-center w-full">
+                          <button onClick={() => addSection()} type="submit" className="mt-3 inline-flex justify-center border border-transparent bg-[#8d0509] py-2 px-3 text-sm font-medium text-white shadow-sm hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2">
+                            Track this section
+                          </button>
+                        </div>
                       </form>
-                  )}
+                    )}
                   </Menu.Item>
                 </div>
               </Menu.Items>
@@ -129,69 +101,71 @@ const Dashboard = () => {
             <button className="text-[#8d0509] text-sm font-medium hover:underline">
               Edit
             </button>
+          </div>
+        </div>
+      </div>
+      <div className="mt-[2%] mb-[2%] px-[5%] flex">
+        <div className="flex justify-center w-full">
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[100px]">CRN</TableHead>
+                <TableHead>Term</TableHead>
+                <TableHead>Course</TableHead>
+                <TableHead>Title</TableHead>
+                <TableHead>Professor</TableHead>
+                <TableHead className="text-right">Amount of Seats</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {
+                sections.map((section) => (
+                  <TableRow>
+                    <TableCell className="font-medium">{section.crn}</TableCell>
+                    <TableCell>{section.term}</TableCell>
+                    <TableCell>{section.course}</TableCell>
+                    <TableCell>{section.title}</TableCell>
+                    <TableCell>{section.professor}</TableCell>
+                    <TableCell className={`text-right`}>{section.seats.remaining}</TableCell>
+                  </TableRow>
+                ))
+              }
+            </TableBody>
+            <TableFooter>
+              <TableRow>
+                <TableCell colSpan="6" className="py-2">
+                  <Pagination className="justify-end">
+                    <PaginationContent>
+                      <PaginationItem>
+                        <PaginationPrevious href="#" />
+                      </PaginationItem>
+                      <PaginationItem>
+                        <PaginationLink href="#">1</PaginationLink>
+                      </PaginationItem>
+                      <PaginationItem>
+                        <PaginationLink href="#" isActive>
+                          2
+                        </PaginationLink>
+                      </PaginationItem>
+                      <PaginationItem>
+                        <PaginationLink href="#">3</PaginationLink>
+                      </PaginationItem>
+                      <PaginationItem>
+                        <PaginationEllipsis />
+                      </PaginationItem>
+                      <PaginationItem>
+                        <PaginationNext href="#" />
+                      </PaginationItem>
+                    </PaginationContent>
+                  </Pagination>
+                </TableCell>
+              </TableRow>
+            </TableFooter>
+          </Table>
         </div>
       </div>
     </div>
-    <div className= "mt-[2%] mb-[2%] px-[5%] flex">
-      <div className="flex justify-center w-full">
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-[100px]">CRN</TableHead>
-          <TableHead>Term</TableHead>
-          <TableHead>Course</TableHead>
-          <TableHead>Title</TableHead>
-          <TableHead>Professor</TableHead>
-          <TableHead className="text-right">Amount of Seats</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {sections.map((section) => (
-          <TableRow>
-            <TableCell className="font-medium">{section.crn}</TableCell>
-            <TableCell>{section.term}</TableCell>
-            <TableCell>{section.course}</TableCell>
-            <TableCell>{section.title}</TableCell>
-            <TableCell>{section.professor}</TableCell>
-            <TableCell className={`text-right ${(section.seats < 1 ? 'text-red-500' : '')}`}>{section.seats}</TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-      <TableFooter>
-        <TableRow>
-          <TableCell colSpan="6" className = "py-2">
-            <Pagination className = "justify-end">
-              <PaginationContent>
-                <PaginationItem>
-                  <PaginationPrevious href="#" />
-                </PaginationItem>
-                <PaginationItem>
-                  <PaginationLink href="#">1</PaginationLink>
-                </PaginationItem>
-                <PaginationItem>
-                  <PaginationLink href="#" isActive>
-                    2
-                  </PaginationLink>
-                </PaginationItem>
-                <PaginationItem>
-                  <PaginationLink href="#">3</PaginationLink>
-                </PaginationItem>
-                <PaginationItem>
-                  <PaginationEllipsis />
-                </PaginationItem>
-                <PaginationItem>
-                  <PaginationNext href="#" />
-                </PaginationItem>
-              </PaginationContent>
-            </Pagination>
-          </TableCell>
-        </TableRow>
-      </TableFooter>
-    </Table>
-    </div>
-    </div>
-    </div>
-  ); 
+  );
 }
 
 export default Dashboard;
