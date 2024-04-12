f36a8a53690c63bc83e04deedbd758a812c9d070
Peter <49101240+peterphann@users.noreply.github.com>
2024-04-12 16:59:18 -0500

    Add loading icon for dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -22,27 +22,27 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
+import LoadingCircle from "../components/LoadingCircle";
 
 const Dashboard = () => {
 
-  const navigate = useNavigate()
-  const [crnInput, setCrnInput] = useState("")
-  const [crns, setCrns] = useState([47550, 10937, 49444, 57605, 58223, 52796, 57519])
-  const [sections, setSections] = useState([])
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(true);
+  const [crnInput, setCrnInput] = useState("");
+  const [crns, setCrns] = useState([47550, 10937, 49444, 57605, 58223, 52796, 57519]);
+  const [sections, setSections] = useState([]);
 
   const dashboardTest = () => {
-    const db = getDatabase()
-
-  }
+    const db = getDatabase();
+  };
 
   const addSection = () => {
-    const crnAdd = parseInt(crnInput)
-    console.log(crns.includes(crnAdd))
+    const crnAdd = parseInt(crnInput);
+    console.log(crns.includes(crnAdd));
     if (!crns.includes(crnAdd)) {
-      setCrns(old => [...old, crnAdd])
+      setCrns(old => [...old, crnAdd]);
     }
-    
-  }
+  };
 
   useEffect(() => {
     getAuth().onAuthStateChanged((user) => {
@@ -53,22 +53,23 @@ const Dashboard = () => {
         navigate('/')
       }
     })
-  }, [])
+  }, []);
 
   useEffect(() => {
     const fetchData = async () => {
       const responses = await Promise.all(
         crns.map(async (crn) => {
           const response = await fetch(`http://localhost:8080/sections/202431/${crn}/`);
-          return response.json()
+          return response.json();
         })
       );
 
-      setSections(responses)
-    }
+      setSections(responses);
+      setIsLoading(false);
+    };
 
-    fetchData()
-  }, [crns])
+    fetchData();
+  }, [crns]);
 
   return (
     <div>
@@ -104,7 +105,8 @@ const Dashboard = () => {
           </div>
         </div>
       </div>
-      <div className="mt-[2%] mb-[2%] px-[5%] flex">
+
+      {!isLoading && <div className="mt-[2%] mb-[2%] px-[5%] flex">
         <div className="flex justify-center w-full">
           <Table>
             <TableHeader>
@@ -163,7 +165,15 @@ const Dashboard = () => {
             </TableFooter>
           </Table>
         </div>
-      </div>
+      </div>}
+
+      {isLoading &&
+        <div className="flex flex-row justify-center mt-8">
+          <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-black" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+          </svg>
+        </div>}
     </div>
   );
 }
