35018acdc925a11d67607b2a87011f75c19c70f1
Peter <49101240+peterphann@users.noreply.github.com>
2024-04-12 05:19:59 -0500

    Begin work on instructor scraping

--- server/section.py
+++ server/section.py
@@ -1,17 +1,35 @@
 from bs4 import BeautifulSoup
 import requests
 
-def parse_soup(soup : BeautifulSoup) -> dict:
+error_json = lambda crn : {
+  'seats' : {
+        'actual' : 'ERROR',
+        'capacity' : 'ERROR',
+        'remaining' : 'ERROR'
+      },
+      'crn' : crn,
+      'title' : 'ERROR',
+      'course' : 'ERROR',
+      'section' : 'ERROR',
+      'term' : 'ERROR',
+      'professor' : 'ERROR'
+}
+
+def parse_soup(soup : BeautifulSoup, term, crn) -> dict:
   all_fields = soup.find_all('td', class_='dddefault')
 
   if len(all_fields) == 0:
     return {}
   
   static_headers = soup.find('div', class_='staticheaders').text
-  term = static_headers.split('\n')[1]
+  term_and_campus = static_headers.split('\n')[1]
+  full_term = ' '.join(term_and_campus.split()[:2])
   full_course_name = soup.find('th', class_='ddlabel').text
   split_name = full_course_name.split(' - ')
 
+  course = split_name[2]
+
+  professor = scrape_instructor(split_name[2], term, crn)
   return {
       'seats' : {
         'actual' : int(all_fields[2].text),
@@ -20,17 +38,19 @@ def parse_soup(soup : BeautifulSoup) -> dict:
       },
       'crn' : int(split_name[1]),
       'title' : split_name[0],
-      'course' : split_name[2],
+      'course' : course,
       'section' : int(split_name[3]),
-      'term' : term
+      'term' : full_term,
+      'professor' : professor
   }
 
-def scrape_instructor(term, crn) -> str:
-  instructor_url = f'https://compass-ssb.tamu.edu/pls/PROD/bwykschd.p_disp_listcrse?term_in={term}&subj_in={self.properties["subject"]}&crse_in={self.properties["course"]}&crn_in={self.properties["crn"]}'
+def scrape_instructor(course, term, crn) -> str:
+  subject = course.split()[0]
+  number = course.split()[1]
+  instructor_url = f'https://compass-ssb.tamu.edu/pls/PROD/bwykschd.p_disp_listcrse?term_in={term}&subj_in={subject}&crse_in={number}&crn_in={crn}'
 
 def scrape_section(term, crn) -> dict:
   url = f'https://compass-ssb.tamu.edu/pls/PROD/bwykschd.p_disp_detail_sched?term_in={term}&crn_in={crn}'
-  print(url)
 
   try:
     page = requests.get(url)
@@ -42,6 +62,6 @@ def scrape_section(term, crn) -> dict:
     return {}
   
   soup = BeautifulSoup(page.text, 'html.parser')
-  course_info = parse_soup(soup)
+  course_info = parse_soup(soup, term, crn)
   return course_info
   
