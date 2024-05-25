9bb2a0cc1ab3e3c820ae7395210fde7a88997ec4
Peter <49101240+peterphann@users.noreply.github.com>
2024-05-25 18:34:54 -0500

    Fix formatting

--- server/server.py
+++ server/server.py
@@ -4,16 +4,18 @@ from section import scrape_section
 
 app = Flask(__name__, static_folder='')
 
+
 @app.route('/sections/<term>/<crn>/', methods=['GET'])
 @cross_origin(origin='*')
 def sections(term, crn):
-  course_info = scrape_section(term, crn)
+    course_info = scrape_section(term, crn)
 
-  if course_info:
-    return course_info
-  else:
-    return Response(f'{{"error": "Course not found", "crn": {crn}, "status": 400}}', status=400, mimetype='application/json')
-  
+    if course_info:
+        return course_info
+    else:
+        return Response(f'{{"error": "Course not found", "crn": {crn}, "status": 400}}', status=400,
+                        mimetype='application/json')
+
 
 if __name__ == "__main__":
-  app.run(debug=True, port=8080)
+    app.run(debug=True, port=8080)
