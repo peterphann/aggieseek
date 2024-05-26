b9764a131b0f35d7bf92ff154dc901e4c6553041
Peter <49101240+peterphann@users.noreply.github.com>
2024-05-25 20:34:32 -0500

    Adjust page headers

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -105,7 +105,7 @@ export default function Navbar() {
                     leaveFrom="transform opacity-100 scale-100"
                     leaveTo="transform opacity-0 scale-95"
                   >
-                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <Menu.Items className="absolute right-0 z-20 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                       {!isUser && <Menu.Item>
                         {({ active }) => (
                           <Link
