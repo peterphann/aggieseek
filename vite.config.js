53d82867a7a4848c5734b1ac87816e0078136bcc
Peter <49101240+peterphann@users.noreply.github.com>
2024-08-01 10:54:08 -0500

    Set proxy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+    '/api': 'http://localhost:8080'
+  }
+  }
 })
