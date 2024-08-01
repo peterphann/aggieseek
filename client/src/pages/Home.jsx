c23bae47046ad960ae4d5b2fca0284aeac303abc
Peter <49101240+peterphann@users.noreply.github.com>
2024-08-01 05:49:14 -0500

    Add home animations

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Logo from "../components/Logo"
 import { Link, useNavigate } from 'react-router-dom'
 import { CheckIcon } from '@heroicons/react/24/outline'
 import ACCBadge from "../components/ACCBadge"
+import { motion } from "framer-motion"
 import { useEffect } from "react"
 import { getAuth } from "firebase/auth"
 
@@ -20,16 +21,23 @@ const Home = () => {
   return (
     <>
       <div className="mt-20 text-center">
-        <div className="flex justify-center">
+        <motion.div className="flex justify-center"
+          initial={{'opacity': 0, translateY: '40%'}}
+          animate={{'opacity': 1, translateY: '0%'}}
+          transition={{type: 'spring', stiffness: 100, duration: 0.5}}>
            <h2 className="text-6xl font-bold">Welcome to </h2>
            <Logo className="object-contain w-80 mx-4" ></Logo>
+        </motion.div>
 
-        </div>
-
-        <h2 className="text-4xl font-bold my-4">Get ahead of the game</h2>
+        <motion.div
+            initial={{'opacity': 0, translateY: '40%'}}
+            animate={{'opacity': 1, translateY: '0%'}}
+            transition={{type: 'spring', stiffness: 100, delay: 0.25, duration: 0.5}}>
+          <h2 className="text-4xl font-bold my-4">Get ahead of the game</h2>
+        </motion.div>
 
         <div className="flex justify-center my-2">
-          <div className="hidden text-base sm:block transform-translate ease-in-out duration-200 mr-5 cursor-default hover:-translate-y-1">
+        <div className="hidden text-base sm:block transform-translate ease-in-out duration-200 mr-5 cursor-default hover:-translate-y-1">
             <CheckIcon className="inline h-8 w-8 pr-2"></CheckIcon>
             Pick what courses you want
           </div>
