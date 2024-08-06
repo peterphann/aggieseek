cc609c299c6916fb373f7dfc117871c2d80753ec
Sophia Phu <sophiatiffphu@gmail.com>
2024-08-05 19:09:26 -0500

    table should be fixed

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -6,7 +6,7 @@ const Table = React.forwardRef(({ className, ...props }, ref) => (
   <div className="relative w-full overflow-auto shadow-xl">
     <table
       ref={ref}
-      className={cn("w-full caption-bottom text-sm", className)}
+      className={cn("w-full caption-bottom text-sm bg-white bg-opacity-25", className)}
       {...props} />
   </div>
 ))
