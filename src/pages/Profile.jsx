41eff61eab08ba083274f0b63189cf86d9b6dfcd
Peter <49101240+peterphann@users.noreply.github.com>
2024-08-07 20:14:55 -0500

    Update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,20 +1,27 @@
-import { Input } from "../components/Input";
-import { Menu } from "@headlessui/react";
-import anonymous from '../assets/profile.webp'
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Switch } from "../components/Switch";
+import {useEffect, useState} from "react";
 import { getAuth } from "firebase/auth";
-import { getDatabase, onValue, ref } from "firebase/database";
+import {getDatabase, onValue, ref, get, set} from "firebase/database";
 import LoadingCircle from "../components/LoadingCircle";
+import InputConfirm from "../components/InputConfirm.jsx";
 
 const Profile = () => {
-  const navigate = useNavigate();
-
-  const [first, setFirst] = useState("");
-  const [last, setLast] = useState("");
-  const [email, setEmail] = useState("");
 
   const [isLoading, setIsLoading] = useState(true);
+
+  const [actualFirst, setActualFirst] = useState('')
+  const [actualLast, setActualLast] = useState('')
+  const [inputFirst, setInputFirst] = useState('')
+  const [inputLast, setInputLast] = useState('')
+
+  const updateSetting = (path, value, setActual) => {
+    const uid = getAuth().currentUser.uid
+    const dbRef = ref(getDatabase(), 'users/' + uid + '/' + path)
+    set(dbRef, value)
+        .then(() => {
+          setActual(value);
+        });
+  }
 
   const fetchFromDatabase = (uid, info) => {
     return new Promise((resolve, reject) => {
@@ -31,137 +38,73 @@ const Profile = () => {
   useEffect(() => {
     getAuth().onAuthStateChanged((user) => {
       if (user) {
-        fetchFromDatabase(user.uid, '/firstName')
-        .then((data) => {
-          setFirst(data);
-        })
-        .catch((error) => {
-          console.log(error);
-        })
-
-        fetchFromDatabase(user.uid, '/lastName')
-        .then((data) => {
-          setLast(data);
-        })
-        .catch((error) => {
-          console.log(error);
+        Promise.all([
+          fetchFromDatabase(user.uid, '/firstName/')
+              .then(data => {
+                setActualFirst(data)
+              }),
+          fetchFromDatabase(user.uid, '/lastName/')
+              .then(data => {
+                setActualLast(data)
+              })
+        ]).then(() => {
+          setIsLoading(false)
         })
 
-        fetchFromDatabase(user.uid, '/email')
-        .then((data) => {
-          setEmail(data);
-          setIsLoading(false);
-        })
-        .catch((error) => {
-          console.log(error);
-        })
-      } else {
-        navigate('/');
       }
-    });
-
+    })
   }, []);
 
+  useEffect(() => {
+    setInputFirst(actualFirst);
+  }, [actualFirst]);
+
+  useEffect(() => {
+      setInputLast(actualLast);
+    }, [actualLast]);
+
+
   return (
-      <div>
-        <div
-            className="flex justify-center items-center mt-10"> {/* Fullscreen container for vertical & horizontal centering */}
-          <div className="flex items-center justify-center w-full max-w-4xl px-4 my-4 "> {/* Content container */}
-            <h2 className="text-3xl font-bold absolute w-full text-center pointer-events-none">Profile</h2> {/* Absolutely positioned to center */}
-
-            <div className="flex flex-row justify-end w-full absolute right-[25%]">
-              <button
-                  className="px-4 z-10 py-2 text-sm font-medium text-[#8d0509] hover:underline focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
-                Save Changes
-              </button>
-            </div>
-
+      <div className="flex justify-center px-2">
+        <div className="flex flex-col w-full max-w-7xl px-8">
+          <div className="flex mt-10">
+            <h2 className="text-3xl font-bold">Profile</h2> {/* Absolutely positioned to center */}
           </div>
-        </div>
 
-        {!isLoading && <div>
-          <div className="px-[15%] flex justify-between mt-5">
+          {!isLoading && <div className="flex justify-between mt-5">
             <div className="flex flex-col">
-              <p className="font-bold text-xl">Personal Information</p>
-              <div className="">
+
+              <div className={"flex"}>
+                <p className="font-bold text-xl">Personal Information</p>
+              </div>
+
+              <div>
                 <p className="text-md font-medium mt-2">First Name</p>
-                <Input placeholder="First Name" defaultValue={first}/>
-                <p className="text-md font-medium mt-2">Last Name</p>
-                <Input placeholder="Last Name" defaultValue={last}/>
-                <p className="text-md font-medium mt-2">Profile Photo</p>
-                <div>
-                  <Menu>
-                    <Menu.Button
-                        className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                      <span className="absolute -inset-1.5"/>
-                      <span className="sr-only">Open user menu</span>
-                      <img
-                          className="h-10 w-10 rounded-full"
-                          src={anonymous}
-                          alt=""
-                      />
-                    </Menu.Button>
-                    <Menu.Items
-                        className="w-[20%] absolute mt-2 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-50">
-                      <div className="px-1 py-1 ">
-                        <Menu.Item>
-                          {({active}) => (
-                              <div
-                                  className={`w-full rounded-md px-0 text-sm text-gray-900`}
-                              >
-                                <Input type="file" onClick={(e) => e.stopPropagation()}
-                                       className="h-10 shadow-none px-1"/>
-                                <button
-                                    className={`${active ? 'bg-red-100' : ''
-                                    } group mt-1 flex w-full items-center rounded-md px-2 py-2 text-sm text-gray-900`}
-                                >
-                                  Change Profile Photo
-                                </button>
-                              </div>
-                          )}
-                        </Menu.Item>
-                        <Menu.Item>
-                          {({active}) => (
-                              <button
-                                  className={`${active ? 'bg-red-100' : ''
-                                  } group mt-1 flex w-full items-center rounded-md px-2 py-2 text-sm text-gray-900`}
-                              >
-                                Delete Profile Photo
-                              </button>
-                          )}
-                        </Menu.Item>
-                      </div>
-                    </Menu.Items>
-                  </Menu>
+                <div className={"flex items-center"}>
+                  <InputConfirm type={"text"} onClick={() => updateSetting('firstName/', inputFirst, setActualFirst)}
+                                confirm={actualFirst !== inputFirst} value={inputFirst}
+                                onChange={(e) => setInputFirst(e.target.value)} placeholder="John"/>
                 </div>
+
+                <p className="text-md font-medium mt-2">Last Name</p>
+                <div className={"flex items-center"}>
+                  <InputConfirm type={"text"} onClick={() => updateSetting('lastName/', inputLast, setActualLast)}
+                                confirm={actualLast !== inputLast} value={inputLast}
+                                onChange={(e) => setInputLast(e.target.value)}  placeholder="Doe"/>
+                </div>
+
               </div>
             </div>
-          </div>
-          <div className="flex justify-center mt-4 mb-4">
-            <hr className="border-gray-300 w-[70%]"/>
-            {/* Adjusted horizontal line */}
-          </div>
-          <div className="px-[15%] flex justify-between">
-            <div className="flex flex-col">
-              <p className="font-bold text-xl">Security</p>
-              <div className="">
-                <p className="text-md font-medium mt-2">Email</p>
-                <Input placeholder="example@domain.com" defaultValue={email}/>
-                <p className="text-md font-medium mt-2">Password</p>
-                <Input placeholder="Password"/>
-                <p className="text-md font-medium mt-2">Confirm Password</p>
-                <Input placeholder="Confirm Password"/>
-              </div>
-            </div>
-          </div>
-        </div>}
 
-        {isLoading &&
-            <div className="flex flex-row justify-center mt-8">
-              <LoadingCircle></LoadingCircle>
-            </div>}
+          </div>}
 
+          {isLoading &&
+              <div className="flex flex-row justify-center mt-8">
+                <LoadingCircle></LoadingCircle>
+              </div>}
+        </div>
       </div>
+
   );
 }
 
