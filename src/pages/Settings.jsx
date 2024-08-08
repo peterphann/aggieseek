a09fa85172afb05ebab72f754c023d6cf54dadc3
Peter <49101240+peterphann@users.noreply.github.com>
2024-08-07 19:53:51 -0500

    Add loading circle to Settings.jsx

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -5,7 +5,6 @@ import {Listbox} from "@headlessui/react";
 import { getAuth } from "firebase/auth";
 import {getDatabase, onValue, ref, get, set} from "firebase/database";
 import LoadingCircle from "../components/LoadingCircle";
-import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
 import InputConfirm from "../components/InputConfirm.jsx";
 
 const Settings = () => {
@@ -14,6 +13,7 @@ const Settings = () => {
   const [selectedTimeZone, setSelectedTimeZone] = useState(timeZones[0]);
   const notifyMode = ["Seats open up", "Any change occurs"]
   const [selectedNotifyMode, setSelectedNotifyMode] = useState(notifyMode[0]);
+  const [isLoading, setIsLoading] = useState(true);
 
 
   const [actualPhone, setActualPhone] = useState('')
@@ -32,12 +32,11 @@ const Settings = () => {
     set(dbRef, value);
   }
 
-  const fetchFromDatabase = (uid, info, set) => {
+  const fetchFromDatabase = (uid, info) => {
     return new Promise((resolve, reject) => {
       const dbRef = ref(getDatabase(), 'users/' + uid + info);
       onValue(dbRef, (snapshot) => {
         const data = snapshot.val();
-        set(data);
         resolve(data);
       }, (error) => {
         reject(error);
@@ -48,12 +47,26 @@ const Settings = () => {
   useEffect(() => {
     getAuth().onAuthStateChanged((user) => {
       if (user) {
-        fetchFromDatabase(user.uid, '/methods/email/value', setActualEmail);
-        fetchFromDatabase(user.uid, '/methods/discord/value', setActualDiscord)
-        fetchFromDatabase(user.uid, '/methods/phone/value', setActualPhone)
-        fetchFromDatabase(user.uid, '/methods/email/enabled', setUsingEmail)
-        fetchFromDatabase(user.uid, '/methods/discord/enabled', setUsingDiscord)
-        fetchFromDatabase(user.uid, '/methods/phone/enabled', setUsingPhone)
+        Promise.all([
+          fetchFromDatabase(user.uid, '/methods/email/')
+              .then(data => {
+                setActualEmail(data['value'])
+                setUsingEmail(data['enabled'])
+              }),
+          fetchFromDatabase(user.uid, '/methods/discord/')
+              .then(data => {
+                setActualDiscord(data['value'])
+                setUsingDiscord(data['enabled'])
+              }),
+          fetchFromDatabase(user.uid, '/methods/phone/')
+              .then(data => {
+                setActualPhone(data['value'])
+                setUsingPhone(data['enabled'])
+               })
+        ]).then(r => {
+          setIsLoading(false)
+        })
+
       }
     })
   }, []);
@@ -71,53 +84,67 @@ const Settings = () => {
     }, [actualDiscord]);
 
   return (
-    <div className="flex justify-center px-2">
-      <div className="flex flex-col w-full max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div className="flex mt-10">
+      <div className="flex justify-center px-2">
+        <div className="flex flex-col w-full max-w-7xl px-8">
+          <div className="flex mt-10">
             <h2 className="text-3xl font-bold">Settings</h2> {/* Absolutely positioned to center */}
-        </div>
-
-
-        <div className="flex justify-between mt-5">
-          <div className="flex flex-col">
-
-            <div className={"flex"}>
-              <p className="font-bold text-xl">Contact Information</p>
-            </div>
-
-            <div className="">
-              <p className="text-md font-medium mt-2">Phone</p>
-              <div className={"flex items-center"}>
-                <InputConfirm type={"tel"} onClick={() => updateSetting('methods/phone/value', inputPhone)}
-                              confirm={actualPhone !== inputPhone} value={inputPhone}
-                              onChange={(e) => setInputPhone(e.target.value)} placeholder="123-456-7890"/>
-                <Switch checked={isUsingPhone} className={"ml-6"}
-                        onCheckedChange={() => {setUsingPhone(!isUsingPhone); updateSetting('methods/phone/enabled', !isUsingPhone)}}/>
-              </div>
-
-              <p className="text-md font-medium mt-2">Email</p>
-              <div className={"flex items-center"}>
-                <InputConfirm type={"email"} disabled onClick={() => updateSetting('methods/email/value', inputEmail)}
-                              confirm={actualEmail !== inputEmail} value={inputEmail}
-                              placeholder="example@domain.com"/>
-                <Switch checked={isUsingEmail} className={"ml-6"}
-                        onCheckedChange={() => {setUsingEmail(!isUsingEmail); updateSetting('methods/email/enabled', !isUsingEmail)}}/>
-              </div>
-
-              <p className="text-md font-medium mt-2">Discord Webhook</p>
-              <div className={"flex items-center"}>
-                <InputConfirm type={"url"} onClick={() => updateSetting('methods/discord/value', inputDiscord)}
-                              confirm={actualDiscord !== inputDiscord} value={inputDiscord}
-                              onChange={(e) => setInputDiscord(e.target.value)} placeholder="Webhook URL"/>
-                <Switch checked={isUsingDiscord} className={"ml-6"}
-                        onCheckedChange={() => {setUsingDiscord(!isUsingDiscord); updateSetting('methods/discord/enabled', !isUsingDiscord)}}/>
-              </div>
-            </div>
           </div>
 
+          {!isLoading && <div className="flex justify-between mt-5">
+            <div className="flex flex-col">
+
+              <div className={"flex"}>
+                <p className="font-bold text-xl">Contact Information</p>
+              </div>
+
+              <div className="">
+                <p className="text-md font-medium mt-2">Phone</p>
+                <div className={"flex items-center"}>
+                  <InputConfirm type={"tel"} onClick={() => updateSetting('methods/phone/value', inputPhone)}
+                                confirm={actualPhone !== inputPhone} value={inputPhone}
+                                onChange={(e) => setInputPhone(e.target.value)} placeholder="123-456-7890"/>
+                  <Switch checked={isUsingPhone} className={"ml-6"}
+                          onCheckedChange={() => {
+                            setUsingPhone(!isUsingPhone);
+                            updateSetting('methods/phone/enabled', !isUsingPhone)
+                          }}/>
+                </div>
+
+                <p className="text-md font-medium mt-2">Email</p>
+                <div className={"flex items-center"}>
+                  <InputConfirm type={"email"} disabled onClick={() => updateSetting('methods/email/value', inputEmail)}
+                                confirm={actualEmail !== inputEmail} value={inputEmail}
+                                placeholder="example@domain.com"/>
+                  <Switch checked={isUsingEmail} className={"ml-6"}
+                          onCheckedChange={() => {
+                            setUsingEmail(!isUsingEmail);
+                            updateSetting('methods/email/enabled', !isUsingEmail)
+                          }}/>
+                </div>
+
+                <p className="text-md font-medium mt-2">Discord Webhook</p>
+                <div className={"flex items-center"}>
+                  <InputConfirm type={"url"} onClick={() => updateSetting('methods/discord/value', inputDiscord)}
+                                confirm={actualDiscord !== inputDiscord} value={inputDiscord}
+                                onChange={(e) => setInputDiscord(e.target.value)} placeholder="Webhook URL"/>
+                  <Switch checked={isUsingDiscord} className={"ml-6"}
+                          onCheckedChange={() => {
+                            setUsingDiscord(!isUsingDiscord);
+                            updateSetting('methods/discord/enabled', !isUsingDiscord)
+                          }}/>
+                </div>
+              </div>
+            </div>
+
+          </div>}
+
+          {isLoading &&
+              <div className="flex flex-row justify-center mt-8">
+                <LoadingCircle></LoadingCircle>
+              </div>}
         </div>
       </div>
-      </div>
+
   );
 }
 
