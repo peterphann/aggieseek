e33e0f89d3238f827c41e497a0140673a4ad10dd
Peter <49101240+peterphann@users.noreply.github.com>
2024-08-17 03:43:01 -0500

    Move user database updates to custom hook

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,30 +3,38 @@ import {useEffect, useState} from "react";
 import { getAuth } from "firebase/auth";
 import {getDatabase, onValue, ref, get, set} from "firebase/database";
 import LoadingCircle from "../components/LoadingCircle";
-import InputConfirm from "../components/InputConfirm.jsx";
+import InputUndo from "../components/InputUndo.jsx";
 import Button from "../components/Button.jsx";
+import useUpdate from "../hooks/useUpdate.jsx";
+import {usePopup} from "../contexts/PopupContext.jsx";
 
 const Profile = () => {
 
-  const [isLoading, setIsLoading] = useState(true);
+    const [isLoading, setIsLoading] = useState(true);
 
-  const [actualFirst, setActualFirst] = useState('')
-  const [actualLast, setActualLast] = useState('')
-  const [actualEmail, setActualEmail] = useState('')
-  const [inputFirst, setInputFirst] = useState('')
-  const [inputLast, setInputLast] = useState('')
-  const [inputEmail, setInputEmail] = useState('')
+    const [actualFirst, setActualFirst] = useState('')
+    const [actualLast, setActualLast] = useState('')
+    const [actualEmail, setActualEmail] = useState('')
+    const [inputFirst, setInputFirst] = useState('')
+    const [inputLast, setInputLast] = useState('')
+    const [inputEmail, setInputEmail] = useState('')
 
-  const updateSetting = (path, value, setActual) => {
-    const uid = getAuth().currentUser.uid
-    const dbRef = ref(getDatabase(), 'users/' + uid + '/' + path)
-    set(dbRef, value)
-        .then(() => {
-          setActual(value);
-        });
-  }
+    const { updateSetting } = useUpdate()
+    const { setPopup } = usePopup()
 
-  const fetchFromDatabase = (uid, info) => {
+    const updateAllSettings = () => {
+        Promise.all([
+            updateSetting('/firstName', inputFirst, setActualFirst),
+            updateSetting('/lastName', inputLast, setActualLast),
+            updateSetting('/email', inputEmail, setActualEmail)
+        ])
+            .then(() => setPopup('Changes saved!'))
+            .catch((error) => {
+                setPopup('An error has occurred.')
+            })
+    }
+
+    const fetchFromDatabase = (uid, info) => {
     return new Promise((resolve, reject) => {
       const dbRef = ref(getDatabase(), 'users/' + uid + info);
       onValue(dbRef, (snapshot) => {
@@ -79,60 +87,66 @@ const Profile = () => {
       <div className="flex justify-center px-2">
         <div className="flex flex-col w-full max-w-7xl px-8">
           <div className="flex mt-10">
-            <h2 className="text-3xl font-bold">Profile</h2> {/* Absolutely positioned to center */}
+            <h2 className="text-3xl font-bold">Profile</h2>
           </div>
 
-          {!isLoading && <div className="flex justify-between mt-5">
-            <div className="flex flex-col w-full">
+            {!isLoading && <>
+                <div className="flex justify-between mt-5">
+                    <div className="flex flex-col w-full">
 
-              <p className="font-bold text-xl">Personal Information</p>
+                        <p className="font-bold text-xl">Personal Information</p>
 
-              <div>
-                <p className="text-md font-medium mt-2">First Name</p>
-                <div className={"flex items-center"}>
-                  <InputConfirm type={"text"} onClick={() => updateSetting('firstName/', inputFirst, setActualFirst)}
-                                confirm={actualFirst !== inputFirst} value={inputFirst}
-                                onChange={(e) => setInputFirst(e.target.value)} placeholder="John"/>
+                        <div>
+                            <p className="text-md font-medium mt-2">First Name</p>
+                            <div className={"flex items-center"}>
+                                <InputUndo type={"text"}
+                                           actual={actualFirst} setValue={setInputFirst} value={inputFirst}
+                                           onChange={(e) => setInputFirst(e.target.value)} placeholder="John"/>
+                            </div>
+
+                            <p className="text-md font-medium mt-2">Last Name</p>
+                            <div className={"flex items-center"}>
+                                <InputUndo type={"text"}
+                                           actual={actualLast} setValue={setInputLast} value={inputLast}
+                                           onChange={(e) => setInputLast(e.target.value)} placeholder="Doe"/>
+                            </div>
+
+                            <p className="text-md font-medium mt-2">Email</p>
+                            <div className={"flex items-center"}>
+                                <InputUndo type={"email"} disabled
+                                           actual={actualEmail} setValue={setInputEmail} value={inputEmail}
+                                           onChange={(e) => setInputEmail(e.target.value)}
+                                           placeholder="example@gmail.com"/>
+                            </div>
+
+                            <div className={"mt-8"}>
+                                <Button onClick={() => updateAllSettings()}>Save Changes</Button>
+                            </div>
+
+                            <hr className={"my-8"}></hr>
+
+                            <p className="font-bold text-xl">Security</p>
+
+                            <div className={"flex"}>
+                                <Button disabled className={"bg-red-600 mt-3"}>
+                                    Change Password
+                                </Button>
+
+                                <Button disabled className={"bg-red-600 mt-3 ml-6"}>
+                                    Delete Account
+                                </Button>
+
+                            </div>
+                        </div>
+                    </div>
                 </div>
+            </>
+            }
 
-                <p className="text-md font-medium mt-2">Last Name</p>
-                <div className={"flex items-center"}>
-                  <InputConfirm type={"text"} onClick={() => updateSetting('lastName/', inputLast, setActualLast)}
-                                confirm={actualLast !== inputLast} value={inputLast}
-                                onChange={(e) => setInputLast(e.target.value)} placeholder="Doe"/>
-                </div>
-
-                <p className="text-md font-medium mt-2">Email</p>
-                <div className={"flex items-center"}>
-                  <InputConfirm type={"email"}
-                                disabled value={inputEmail}
-                                onChange={(e) => setInputEmail(e.target.value)} placeholder="example@gmail.com"/>
-                </div>
-
-                <hr className={"my-8"}></hr>
-
-                <p className="font-bold text-xl">Security</p>
-
-                <div className={"flex"}>
-                  <Button disabled className={"bg-red-600 mt-3"}>
-                    Change Password
-                  </Button>
-
-                  <Button disabled className={"bg-red-600 mt-3 ml-6"}>
-                    Delete Account
-                  </Button>
-
-                </div>
-
-              </div>
-            </div>
-
-          </div>}
-
-          {isLoading &&
-              <div className="flex flex-row justify-center mt-8">
-                <LoadingCircle></LoadingCircle>
-              </div>}
+            {isLoading &&
+                <div className="flex flex-row justify-center mt-8">
+                    <LoadingCircle></LoadingCircle>
+                </div>}
         </div>
       </div>
 
--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -3,16 +3,16 @@ import {useEffect, useState} from "react";
 import { getAuth } from "firebase/auth";
 import {getDatabase, onValue, ref, get, set} from "firebase/database";
 import LoadingCircle from "../components/LoadingCircle";
-import InputConfirm from "../components/InputConfirm.jsx";
+import {Input} from "../components/Input.jsx";
+import Button from "../components/Button.jsx";
+import {usePopup} from "../contexts/PopupContext.jsx";
+import DebugPane from "../components/DebugPane.jsx";
+import InputUndo from "../components/InputUndo.jsx";
+import useUpdate from "../hooks/useUpdate.jsx";
 
 const Settings = () => {
 
-  const timeZones = ["Pacific Time", "Eastern Time", "Central Time"];
-  const [selectedTimeZone, setSelectedTimeZone] = useState(timeZones[0]);
-  const notifyMode = ["Seats open up", "Any change occurs"]
-  const [selectedNotifyMode, setSelectedNotifyMode] = useState(notifyMode[0]);
   const [isLoading, setIsLoading] = useState(true);
-
 
   const [actualPhone, setActualPhone] = useState('')
   const [actualEmail, setActualEmail] = useState('')
@@ -24,13 +24,19 @@ const Settings = () => {
   const [isUsingEmail, setUsingEmail] = useState(false)
   const [isUsingDiscord, setUsingDiscord] = useState(false)
 
-  const updateSetting = (path, value, setActual) => {
-    const uid = getAuth().currentUser.uid
-    const dbRef = ref(getDatabase(), 'users/' + uid + '/' + path)
-    set(dbRef, value)
-        .then(() => {
-            setActual(value)
-        });
+    const { setPopup } = usePopup()
+    const { updateSetting } = useUpdate()
+
+  const updateAllSettings = () => {
+      Promise.all([
+          updateSetting('/methods/phone/value', inputPhone, setActualPhone),
+          updateSetting('/methods/email/value', inputEmail, setActualEmail),
+          updateSetting('/methods/discord/value', inputDiscord, setActualDiscord)
+      ])
+          .then(() => setPopup('Changes saved!'))
+          .catch((error) => {
+              setPopup('An error has occurred.')
+          })
   }
 
   const fetchFromDatabase = (uid, info) => {
@@ -46,7 +52,7 @@ const Settings = () => {
   };
 
   useEffect(() => {
-    getAuth().onAuthStateChanged((user) => {
+      getAuth().onAuthStateChanged((user) => {
       if (user) {
         Promise.all([
           fetchFromDatabase(user.uid, '/methods/email/')
@@ -85,66 +91,77 @@ const Settings = () => {
     }, [actualDiscord]);
 
   return (
-      <div className="flex justify-center px-2">
-        <div className="flex flex-col w-full max-w-7xl px-8">
-          <div className="flex mt-10">
-            <h2 className="text-3xl font-bold">Settings</h2> {/* Absolutely positioned to center */}
+      <>
+          <div className="flex justify-center px-2">
+              <div className="flex flex-col w-full max-w-7xl px-8">
+                  <div className="flex mt-10">
+                      <h2 className="text-3xl font-bold">Settings</h2>
+                  </div>
+
+                  {!isLoading && <>
+                      <div className="flex justify-between mt-5">
+                          <div className="flex flex-col">
+
+                              <div className={"flex"}>
+                                  <p className="font-bold text-xl">Contact Information</p>
+                              </div>
+
+                              <div className="">
+                                  <p className="text-md font-medium mt-2">Phone</p>
+                                  <div className={"flex items-center"}>
+                                      <InputUndo type={"tel"} placeholder="123-456-7890"
+                                                 value={inputPhone}  setValue={setInputPhone} actual={actualPhone}
+                                                 onChange={(e) => setInputPhone(e.target.value)}></InputUndo>
+                                      <Switch checked={isUsingPhone} className={"ml-6"}
+                                              onCheckedChange={() => {
+                                                  setUsingPhone(!isUsingPhone);
+                                                  updateSetting('methods/phone/enabled', !isUsingPhone)
+                                              }}/>
+                                  </div>
+
+                                  <p className="text-md font-medium mt-2">Email</p>
+                                  <div className={"flex items-center"}>
+                                      <InputUndo type={"email"} placeholder="example@domain.com" disabled
+                                                 value={inputEmail}  setValue={setInputEmail} actual={actualEmail}
+                                                 onChange={(e) => setInputEmail(e.target.value)}></InputUndo>
+                                      <Switch checked={isUsingEmail} className={"ml-6"} disabled
+                                              onCheckedChange={() => {
+                                                  setUsingEmail(!isUsingEmail);
+                                                  updateSetting('methods/email/enabled', !isUsingEmail)
+                                              }}/>
+                                  </div>
+
+                                  <p className="text-md font-medium mt-2">Discord Webhook</p>
+                                  <div className={"flex items-center"}>
+                                      <InputUndo type={"url"} placeholder="Webhook URL"
+                                                 value={inputDiscord}  setValue={setInputDiscord} actual={actualDiscord}
+                                                 onChange={(e) => setInputDiscord(e.target.value)}></InputUndo>
+                                      <Switch className={"ml-6"}
+                                              checked={isUsingDiscord}
+                                              onCheckedChange={() => {
+                                                  setUsingDiscord(!isUsingDiscord);
+                                                  updateSetting('methods/discord/enabled', !isUsingDiscord)
+                                              }}/>
+                                  </div>
+                              </div>
+
+                          </div>
+                      </div>
+
+                      <div className={"mt-8"}>
+                          <Button onClick={() => updateAllSettings()}>Save Changes</Button>
+                      </div>
+                  </>}
+
+                  {isLoading &&
+                      <div className="flex flex-row justify-center mt-8">
+                          <LoadingCircle></LoadingCircle>
+                      </div>}
+              </div>
           </div>
 
-          {!isLoading && <div className="flex justify-between mt-5">
-            <div className="flex flex-col">
 
-              <div className={"flex"}>
-                <p className="font-bold text-xl">Contact Information</p>
-              </div>
-
-              <div className="">
-                <p className="text-md font-medium mt-2">Phone</p>
-                <div className={"flex items-center"}>
-                  <InputConfirm type={"tel"} onClick={() => updateSetting('methods/phone/value', inputPhone, setActualPhone)}
-                                confirm={actualPhone !== inputPhone} value={inputPhone}
-                                onChange={(e) => setInputPhone(e.target.value)} placeholder="123-456-7890"/>
-                  <Switch checked={isUsingPhone} className={"ml-6"}
-                          onCheckedChange={() => {
-                            setUsingPhone(!isUsingPhone);
-                            updateSetting('methods/phone/enabled', !isUsingPhone)
-                          }}/>
-                </div>
-
-                <p className="text-md font-medium mt-2">Email</p>
-                <div className={"flex items-center"}>
-                  <InputConfirm type={"email"} disabled onClick={() => updateSetting('methods/email/value', inputEmail, setActualEmail)}
-                                confirm={actualEmail !== inputEmail} value={inputEmail}
-                                placeholder="example@domain.com"/>
-                  <Switch checked={isUsingEmail} className={"ml-6"}
-                          onCheckedChange={() => {
-                            setUsingEmail(!isUsingEmail);
-                            updateSetting('methods/email/enabled', !isUsingEmail)
-                          }}/>
-                </div>
-
-                <p className="text-md font-medium mt-2">Discord Webhook</p>
-                <div className={"flex items-center"}>
-                  <InputConfirm type={"url"} onClick={() => updateSetting('methods/discord/value', inputDiscord, setActualDiscord())}
-                                confirm={actualDiscord !== inputDiscord} value={inputDiscord}
-                                onChange={(e) => setInputDiscord(e.target.value)} placeholder="Webhook URL"/>
-                  <Switch checked={isUsingDiscord} className={"ml-6"}
-                          onCheckedChange={() => {
-                            setUsingDiscord(!isUsingDiscord);
-                            updateSetting('methods/discord/enabled', !isUsingDiscord)
-                          }}/>
-                </div>
-              </div>
-            </div>
-
-          </div>}
-
-          {isLoading &&
-              <div className="flex flex-row justify-center mt-8">
-                <LoadingCircle></LoadingCircle>
-              </div>}
-        </div>
-      </div>
+      </>
 
   );
 }
