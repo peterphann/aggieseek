4b5877e403c1b08a3ade51da66e6f3b1684a3513
Peter Phan <pet3r.ph4n@gmail.com>
2024-10-21 15:36:14 -0500

    Add aggiered color

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,7 @@ module.exports = {
     },
     extend: {
       colors: {
+        aggiered: "#8d0509",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
