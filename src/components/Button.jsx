4857d8fbc08a5b5ca05dc6bf4c401d89b6eff916
Peter Phan <pet3r.ph4n@gmail.com>
2024-10-29 01:45:40 -0500

    Make buttons rounded

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,7 +6,7 @@ const Button = ({ children, className, ...props }) => {
           className={"inline-block"}
           whileHover={!props.disabled && {translateY: '3%'}}>
         <button
-            className={`transition-transform ease-in-out duration-100 px-6 py-2 bg-aggiered shadow-lg text-white font-bold disabled:cursor-not-allowed ${!props.disabled && "active:scale-95"} ${className}`}
+            className={`rounded-md transition-transform ease-in-out duration-100 px-6 py-2 bg-aggiered shadow-lg text-white font-bold disabled:cursor-not-allowed ${!props.disabled && "active:scale-95"} ${className}`}
             {...props}>
           {children}
         </button>
