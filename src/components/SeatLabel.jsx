f45305bf1ac4de310c4926c38395076bfa576dc5
Peter Phan <pet3r.ph4n@gmail.com>
2024-10-29 01:45:54 -0500

    Create SeatLabel.jsx

---
+++ src/components/SeatLabel.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import LoadingCircle from "./LoadingCircle";
+
+const API_URL = import.meta.env.VITE_API_URL
+const CURRENT_TERM = import.meta.env.VITE_CURRENT_TERM
+
+const SeatLabel = ({ crn, className }) => {
+
+  const [seats, setSeats] = useState(null)
+
+  const fetchSeats = async () => {
+    const response = await fetch(`${API_URL}/terms/${CURRENT_TERM}/classes/${crn}/seats`);
+    const data = await response.json();
+
+    setSeats(data.SEATS);
+  }
+
+  useEffect(() => {
+    fetchSeats()
+  }, [])
+
+  return (
+    <div className={'flex justify-end'}>
+      {seats == null 
+      ? <LoadingCircle />
+      : seats.REMAINING}
+    </div>
+  )
+}
+
+export default SeatLabel;
